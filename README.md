7c71e9e6aa394c157a0e43e1bb0be56181694ef9
kassadube <noamsrb@yahoo.com>
2021-06-29 18:53:03 +0300

    update test

--- README.md
+++ README.md
@@ -8,3 +8,16 @@ https://binance-docs.github.io/apidocs/futures/en/#signed-trade-and-user_data-en
 bot.py
 
 https://github.com/jp-developer0/scalper/blob/main/bot.py
+
+
+time 
+enddate = dt.datetime.now()
+tmd = dt.timedelta(minutes=10)
+startdate = dt.datetime.now() - tmd
+
+print (f"startDate {dt.datetime.timestamp(startdate)}")
+print (f"endDate {dt.datetime.timestamp(dt.datetime.now())}")
+
+date_time = dt.datetime.fromtimestamp(1624965847)
+print(date_time)
+print(dt.datetime.fromtimestamp(1624966447))
--- test.py
+++ test.py
@@ -2,28 +2,37 @@ import config
 from binance.client import Client
 from binance.enums import *
 import time
-import datetime as dt
 import numpy as np
 from IPython.display import display
+import pandas as pd                # working with data frames
+import datetime as dt              # working with dates
+import matplotlib.pyplot as plt    # plot data
+import qgrid                       # display dataframe in notebooks 
 
 
 client = Client(config.API_KEY, config.API_SECRET)
 symbolTicker = 'LINKUSDT'
 
-enddate = dt.datetime.now()
-tmd = dt.timedelta(minutes=10)
-startdate = dt.datetime.now() - tmd
-
-print (f"startDate {dt.datetime.timestamp(startdate)}")
-print (f"endDate {dt.datetime.timestamp(dt.datetime.now())}")
-
-date_time = dt.datetime.fromtimestamp(1624965204.864095)
-print(date_time)
 
 
 
-klines = client.get_historical_klines(symbolTicker, "15m", "1 hour ago UTC",klines_type = HistoricalKlinesType.FUTURES)
-klines2 = client.get_historical_klines(symbolTicker, "15m", dt.datetime.timestamp(startdate), dt.datetime.timestamp(dt.datetime.now()), klines_type = HistoricalKlinesType.FUTURES)
-display(klines2)
 
+#klines = client.get_historical_klines(symbolTicker, "15m", "1 hour ago UTC",klines_type = HistoricalKlinesType.FUTURES)
+klines2 = client.get_historical_klines(symbolTicker, "1m", "5 minutes ago UTC", klines_type = HistoricalKlinesType.FUTURES)
+#display(klines2)
+
+df = pd.DataFrame(klines2)
+df = df.iloc[:, 0:6]
+df.columns = ['datetime', 'open', 'high', 'low', 'close', 'volume']
+
+df.open      = df.open.astype("float")
+df.high      = df.high.astype("float")
+df.low       = df.low.astype("float")
+df.close     = df.close.astype("float")
+df.volume    = df.volume.astype("float")
+df.datetime = [dt.datetime.fromtimestamp(x / 1000.0) for x in df.datetime]
+df.to_csv("fo1.csv")
+display(df)
+print(client.get_symbol_ticker(symbol="LINKUSDT"))
+print(client.futures_symbol_ticker(symbol="LINKUSDT"))
 
